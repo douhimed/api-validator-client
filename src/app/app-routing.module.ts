07f420e4d3b81c808b581ed7e532cb766ee5d610
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-07-26 23:06:50 +0100

    VLD-21 delete project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {NewProjectComponent} from "./components/new-project/new-project.component";
 import {UpdateProjectComponent} from "./components/update-project/update-project.component";
+import {DeleteProjectComponent} from "./components/delete-project/delete-project.component";
 
 const routes: Routes = [
   {path: "new-project", component: NewProjectComponent},
   {path: "update-project/:id", component: UpdateProjectComponent},
+  {path: "delete-project/:id", component: DeleteProjectComponent},
+
 ];
 
 
--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,4 +1,4 @@
 export interface Project {
-  id: number | undefined;
+  id: number;
   name : string;
 }
