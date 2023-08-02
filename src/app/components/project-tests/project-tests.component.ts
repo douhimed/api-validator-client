79e1deb37d81e9deac5cd36b78d3151c38ff84cd
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-08-02 10:07:35 +0100

    [feature]VLD-24: PART 2 send jira ticket

--- src/app/components/project-tests/project-tests.component.ts
+++ src/app/components/project-tests/project-tests.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TestResult } from 'src/app/models/testResult';
 import { ProjectService } from 'src/app/services/project.service';
-import { ResponseDto } from './../../models/ResponseDto';
 import { OperationService } from 'src/app/services/operation.service';
+import { JiraTicketService } from 'src/app/services/jira-ticket.service';
+import { Fields } from 'src/app/models/jira-models/fields';
+import { Messages } from 'src/app/models/Messages';
+import { jiraPayload } from 'src/app/models/jira-models/jiraPayload';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-project-tests',
@@ -14,9 +18,12 @@ export class ProjectTestsComponent implements OnInit {
   id!: number;
   testResult!: TestResult;
 
+  jiraModel: Fields = { summary: '', description: '', project: { key: '' }, issuetype: { id: '' } };
+  jiraPayload:jiraPayload={fields:{ summary: '', description: '', project: { key: '' }, issuetype: { id: '' } }};
+
   isCollapsed = true;
 
-  constructor(private projectService: ProjectService,private operationService: OperationService ,private route: ActivatedRoute) {}
+  constructor(private projectService: ProjectService,private operationService: OperationService ,private jiraTicketService: JiraTicketService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
@@ -41,7 +48,60 @@ export class ProjectTestsComponent implements OnInit {
     );
   }
 
+  //JIRA
+  sendJiraTicket(type:string,url:string,messages:Messages[]) {
+
+    this.jiraModel.summary=`${type} : ${url}`
+    this.jiraModel.project.key=`AP`
+
+    let desc: string = '';
+    for (let i = 0; i < messages.length; i++) {
+      const message = messages[i];
+      desc += `Rapport N°${i} :
+              Operation : ${message.op ?? ''}
+              Path : ${message.path ?? ''}
+              Value : ${message.value?.msg ?? ''}
+              -----------------------------\n`;
+    }
+
+    this.jiraModel.description=`${desc}`
+    this.jiraModel.issuetype.id=`10029`
+
+    this.jiraPayload.fields=this.jiraModel
+
+    this.jiraTicketService.sendJiraTicket(this.jiraPayload).subscribe(
+      (response) => {
+        this.showSuccessAlert();
+        console.log('Jira ticket sent successfully!', response);
+      },
+      (error) => {
+        this.showErrorAlert();
+        console.error('Error sending Jira ticket:', error);
+      }
+    )
+  }
+
   toggleCollapse(responseDto: any) {
     responseDto.isCollapsed = !responseDto.isCollapsed;
   }
+
+  showSuccessAlert() {
+    Swal.fire({
+      icon: 'success',
+      title: 'Success!',
+      text: 'Jira ticket sent successfully!',
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#32CD32',
+    });
+  }
+
+  showErrorAlert() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error!',
+      text: 'Failed to send Jira ticket. Please try again later.',
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#32CD32',
+    });
+}
 }
