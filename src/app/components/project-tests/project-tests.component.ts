03bbff209752b9ce497d4b745d121c050c1146cd
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-08-28 17:06:50 +0100

    [feature]rework-6: Add Chart.js

--- src/app/components/project-tests/project-tests.component.ts
+++ src/app/components/project-tests/project-tests.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { TestResult } from 'src/app/models/testResult';
 import { ProjectService } from 'src/app/services/project.service';
 import { OperationService } from 'src/app/services/operation.service';
@@ -7,8 +7,10 @@ import { JiraTicketService } from 'src/app/services/jira-ticket.service';
 import { Fields } from 'src/app/models/jira-models/fields';
 import { Messages } from 'src/app/models/Messages';
 import { jiraPayload } from 'src/app/models/jira-models/jiraPayload';
-import Swal from 'sweetalert2';
 import { ToastrService } from 'ngx-toastr';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import DatalabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-project-tests',
@@ -16,51 +18,121 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./project-tests.component.css'],
 })
 export class ProjectTestsComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
   id!: number;
   testResult!: TestResult;
 
-  jiraModel: Fields = { summary: '', description: '', project: { key: '' }, issuetype: { id: '' } };
-  jiraPayload:jiraPayload={fields:{ summary: '', description: '', project: { key: '' }, issuetype: { id: '' } }};
+  jiraModel: Fields = {
+    summary: '',
+    description: '',
+    project: { key: '' },
+    issuetype: { id: '' },
+  };
+  jiraPayload: jiraPayload = {
+    fields: {
+      summary: '',
+      description: '',
+      project: { key: '' },
+      issuetype: { id: '' },
+    },
+  };
 
   isCollapsed = true;
+  totalOperations!: number;
+  validCount!: number;
+  invalidCount!: number;
+  otherCount = this.totalOperations - (this.validCount + this.invalidCount);
 
-  constructor(private projectService: ProjectService,private operationService: OperationService , private toastr: ToastrService ,private jiraTicketService: JiraTicketService, private route: ActivatedRoute) {}
+  //constructor
+  constructor(
+    private projectService: ProjectService,
+    private operationService: OperationService,
+    private toastr: ToastrService,
+    private jiraTicketService: JiraTicketService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     this.loadProjectTestResults();
   }
 
+  // Load project test results
   loadProjectTestResults(): void {
     this.projectService.runProjectTest(this.id).subscribe((data) => {
-
-      for(let i=0;i<data.responseDto.length;i++){
-        for(let j=0;j<data.responseDto[i].messages.length;j++){
-          data.responseDto[i].messages[j].value = JSON.stringify(data.responseDto[i].messages[j].value)
+      for (let i = 0; i < data.responseDto.length; i++) {
+        for (let j = 0; j < data.responseDto[i].messages.length; j++) {
+          data.responseDto[i].messages[j].value = JSON.stringify(
+            data.responseDto[i].messages[j].value
+          );
         }
       }
 
-      this.testResult = data
+      this.testResult = data;
+      this.totalOperations = data.responseDto.length;
+      this.validCount = this.getCountByValidationStatus(data, 'VALID');
+      this.invalidCount = this.getCountByValidationStatus(data, 'INVALID');
+      this.otherCount =
+        this.totalOperations - (this.validCount + this.invalidCount);
+
+      // Update pie chart data
+      this.updatePieChartData();
     });
   }
-
-  handleUpdateExpectedResponse(id: number, newExpectedResponse: string): void {
-    this.operationService.updateExcpectedResponse(id, newExpectedResponse).subscribe(
-      (data) => {
-        this.toastr.success('Expected response updated successfully', 'Success');
-        this.loadProjectTestResults();
-      },
-      (error) => {
-        this.toastr.error('Failed to update expected response: '+error.error.message, 'Error');
-      }
-    );
+  // Update pie chart data
+  updatePieChartData(): void {
+    this.pieChartData.datasets[0].data = [
+      this.validCount,
+      this.invalidCount,
+      this.otherCount,
+    ];
   }
 
-  //JIRA
-  sendJiraTicket(type:string,url:string,messages:Messages[]) {
+  // ##########---PIE---##########
+  public pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+      datalabels: {
+        formatter: (value: any, ctx: any) => {
+          if (ctx.chart.data.labels) {
+            return ctx.chart.data.labels[ctx.dataIndex];
+          }
+        },
+      },
+    },
+  };
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: [['VALID'], ['INVALID'], ['BAD REQUEST']],
+    datasets: [
+      {
+        data: [200, 10, 20],
+        backgroundColor: ['#66BB6A', '#EF5350', '#ffcc00'],
+      },
+    ],
+  };
+  public pieChartType: ChartType = 'pie';
+  public pieChartPlugins = [DatalabelsPlugin];
+  changeLegendPosition(): void {
+    if (this.pieChartOptions?.plugins?.legend) {
+      this.pieChartOptions.plugins.legend.position =
+        this.pieChartOptions.plugins.legend.position === 'left'
+          ? 'top'
+          : 'left';
+    }
 
-    this.jiraModel.summary=`${type} : ${url}`
-    this.jiraModel.project.key=`AP`
+    this.chart?.render();
+  }
+  // ##########---PIE---##########
+
+  // ##########---JIRA---##########
+  sendJiraTicket(type: string, url: string, messages: Messages[]) {
+    this.jiraModel.summary = `${type} : ${url}`;
+    this.jiraModel.project.key = `AP`;
 
     let desc: string = '';
     for (let i = 0; i < messages.length; i++) {
@@ -72,10 +144,10 @@ export class ProjectTestsComponent implements OnInit {
               -----------------------------\n`;
     }
 
-    this.jiraModel.description=`${desc}`
-    this.jiraModel.issuetype.id=`10029`
+    this.jiraModel.description = `${desc}`;
+    this.jiraModel.issuetype.id = `10029`;
 
-    this.jiraPayload.fields=this.jiraModel
+    this.jiraPayload.fields = this.jiraModel;
 
     this.jiraTicketService.sendJiraTicket(this.jiraPayload).subscribe(
       (response) => {
@@ -86,7 +158,28 @@ export class ProjectTestsComponent implements OnInit {
         this.showErrorAlert();
         console.error('Error sending Jira ticket:', error);
       }
-    )
+    );
+  }
+  // ##########---JIRA---##########
+
+  handleUpdateExpectedResponse(id: number, newExpectedResponse: string): void {
+    this.operationService
+      .updateExcpectedResponse(id, newExpectedResponse)
+      .subscribe(
+        (data) => {
+          this.toastr.success(
+            'Expected response updated successfully',
+            'Success'
+          );
+          this.loadProjectTestResults();
+        },
+        (error) => {
+          this.toastr.error(
+            'Failed to update expected response: ' + error.error.message,
+            'Error'
+          );
+        }
+      );
   }
 
   toggleCollapse(responseDto: any) {
@@ -99,5 +192,15 @@ export class ProjectTestsComponent implements OnInit {
 
   showErrorAlert() {
     this.toastr.error('Failed to send Jira ticket. Please try again later', '');
-}
+  }
+
+  private getCountByValidationStatus(datas: any, status: string): number {
+    let count = 0;
+    for (const data of datas.responseDto) {
+      if (data.validationStatus === status) {
+        count++;
+      }
+    }
+    return count;
+  }
 }
